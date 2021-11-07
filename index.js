9b0f81ed3cd18133e2ddeb70d901bff862383efa
JoshSee96 <74890550+JoshSee96@users.noreply.github.com>
2021-11-06 20:29:11 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const nodemon = require('nodemon');
+app.use(express.json());
+
+//MongoDB Package
+const mongoose = require('mongoose');
+
+const PORT = 1200;
+
+const dbUrl = "mongodb+srv://admin:Password1@cluster0.broum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+//Connect to MongoDB
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//MongoDB Connection
+const db = mongoose.connection;
+
+//Handle DB Error, display connection
+db.on('error', () => {
+    console.error.bind(console, 'connection error: ');
+});
+db.once('open', () => {
+    console.log('MongoDB Connected');
+});
+
+//Schema/Model Declaration
+require('./Models/studentObject');
+require('./Models/courseObject');
+
+const Student = mongoose.model('Student');
+const Course = mongoose.model('Course');
+
+app.get('/',(req,res) => {
+    return res.status(200).json("(message: OK)");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server Started on Port ${PORT}`);
+});
